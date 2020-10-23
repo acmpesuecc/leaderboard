355bf2fa970f65d4e5583d40b1064ce146fb94f3
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-10-23 15:40:09 +0530

    Update logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
+import logo from './logo.jpeg';
 import './App.css';
 
 function App() {
