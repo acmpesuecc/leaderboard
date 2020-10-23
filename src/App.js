d854a3af3a8b6bfb49e6a3dd9ac25087405eb4d4
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-10-23 15:35:42 +0530

    Basic dash working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [result, setResult] = useState({"No one yet!":"???"});
+  const [dataret, setDataret] = useState(1);
+
+  useEffect(async () => {
+    if(dataret){
+      const fresult = await axios({
+        method: 'get',
+        url: 'https://acm-bounty.herokuapp.com/scores'
+      });
+
+      setResult(fresult.data);
+      console.log(result);
+      setDataret(0);
+    }
+  });
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Leaderboard
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table>
+        <thead>
+        <tr>
+          <th>Rank</th><th>Github Username</th><th>Total Score</th>
+        </tr>
+        </thead>
+        <tbody>
+        {Object.keys(result).map((keyName, i) => (
+            <tr>
+                <td>{i+1}</td><td>{keyName}</td><td>{result[keyName]}</td>
+            </tr>
+        ))}
+        </tbody>
+        </table>
       </header>
     </div>
   );
