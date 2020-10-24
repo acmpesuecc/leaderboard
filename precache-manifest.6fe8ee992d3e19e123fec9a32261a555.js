40bc9b57ba0cd7e938c970ffe73dd737b7a4ba60
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-10-24 21:59:54 +0530

    Updates

---
+++ precache-manifest.6fe8ee992d3e19e123fec9a32261a555.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94230738894c8e2e603113ad804b668e",
+    "url": "/leaderboard/index.html"
+  },
+  {
+    "revision": "aed4bbd390b800f2300e",
+    "url": "/leaderboard/static/css/main.60c1b1d8.chunk.css"
+  },
+  {
+    "revision": "deecccd2e3d6a9707b5a",
+    "url": "/leaderboard/static/js/2.31cd3195.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/leaderboard/static/js/2.31cd3195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed4bbd390b800f2300e",
+    "url": "/leaderboard/static/js/main.92c6c992.chunk.js"
+  },
+  {
+    "revision": "9af617e9c0f4dd0eff03",
+    "url": "/leaderboard/static/js/runtime-main.128e2eef.js"
+  },
+  {
+    "revision": "0458cdea5f3ab84fbdcb8c2313e9ff63",
+    "url": "/leaderboard/static/media/logo.0458cdea.jpeg"
+  }
+]);
