ea8618bb1b00150c1140332722477bdb84bac8a1
anirudhRowjee <ani.rowjee@gmail.com>
2020-10-25 17:38:06 +0530

    Closes #5
    This commit closes Issue #5.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import logo from './logo.jpeg';
+
+import fuzzysearch from 'fuzzysearch';
+
 import './App.css';
 
 function refreshPage() {
@@ -11,6 +14,10 @@ function App() {
 
   const [result, setResult] = useState({"No one yet!":"???"});
   const [dataret, setDataret] = useState(1);
+
+  const [searchQuery, setSeachQuery] = useState("");
+
+  const [fuzzy, setFuzzy] = useState();
 
   useEffect(async () => {
     if(dataret){
@@ -37,9 +44,25 @@ function App() {
   })
 
   // sort the result in descending order of score
-  const sortedResults = result_array.sort((first, second) => {
+  var sortedResults = result_array.sort((first, second) => {
     return second.score - first.score;
   })
+
+  function Search(keyword) {
+    var temp = sortedResults.filter(object => {
+      return fuzzysearch(keyword.toLowerCase(), object.username.toLowerCase());
+    })
+    return temp;
+  }
+
+  function handleSearchBarChange(event){
+    var text = event.target.value;
+    setSeachQuery(text)
+    console.log("searching ");
+    console.log(searchQuery);
+    setFuzzy(Search(searchQuery));
+    console.log(fuzzy);
+  }
 
 
   return (
@@ -50,9 +73,36 @@ function App() {
         <p>
         <h1>Leaderboard</h1>
         </p>
-        <div>
-        <button onClick={refreshPage}>Click to reload!</button>
+        <div className='controls'>
+          <button className="refresh-button" onClick={refreshPage}>Click to reload!</button>
+          <div className='searchbar'>
+            <input type="text" className='search-text' onChange={handleSearchBarChange} placeholder="search by username" />
+            <button className='clearSearch' onClick={() => setFuzzy(null)}> X </button>
+          </div>
         </div>
+
+        {fuzzy && (
+            <table className="table table-dark fuzzy-results">
+              <thead>
+                <tr>
+                  <th>Rank</th><th>Github Username</th><th>Total Score</th>
+                </tr>
+              </thead>
+              <tbody>
+
+                {fuzzy.map((object, counter) => (
+                  <tr key={object.username}>
+                    <td> {counter + 1} </td>
+                    <td><a href = {url+`${object.username}`} target="_blank" rel='noreferrer'>{object.username}</a></td>
+                    <td>{object.score}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+        )}
+
+        <br/>
+
         <table className="table table-dark">
         <thead>
         <tr>
@@ -60,6 +110,7 @@ function App() {
         </tr>
         </thead>
         <tbody>
+
           {sortedResults && sortedResults.map((object, counter) => (
             <tr key={object.username}>
               <td> {counter + 1} </td>
